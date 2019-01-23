e6064a1e1a82b20a45bc572d0d30dc3fd3a860ff
jinhyeok <persona5173@gmail.com>
2019-01-23 21:45:15 +0900

    test

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Hello Jekyll!",
-        "excerpt":"Hello World      My Blog URL :  http://localhost:4000      첫 번째 인용문         두 번째 인용문       test       video  ","categories": ["Test"],
-        "tags": ["first"],
-        "url": "http://localhost:4000/test/first-post/",
-        "teaser":null}]
